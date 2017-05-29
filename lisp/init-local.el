391e2120c69255ef7577dc7bc409014e6a456df4
Regashia <regashia@gmail.com>
2017-05-29 13:04:12 +0900

    Add ddskk
    google-ime-skk という gem を使用する

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -5,4 +5,20 @@
 (when (maybe-require-package 'direnv)
   (direnv-mode))
 
+;; ddskk
+(when (maybe-require-package 'ddskk)
+  ;; google-ime-skk
+  (setq skk-server-prog "google-ime-skk")      ; google-ime-skk の場所
+  (setq skk-server-inhibit-startup-server nil) ; 辞書サーバが起動していなかったときに Emacs からプロセスを立ち上げる
+  (setq skk-server-host "localhost")           ; サーバー機能を利用
+  (setq skk-server-portnum 55100)              ; ポートは google-ime-skk
+  (setq skk-share-private-jisyo t)             ; 複数 skk 辞書を共有
+
+  ;; ミニバッファでは C-j を改行にしない
+  (define-key minibuffer-local-map (kbd "C-j") 'skk-kakutei)
+  ;; ¥ と \ を同じものとして扱う
+  (define-key key-translation-map (kbd "C-¥") (kbd "C-\\"))
+  ;; C-\ で skk-mode 起動
+  (global-set-key "\C-\\" 'skk-mode))
+
 (provide 'init-local)
