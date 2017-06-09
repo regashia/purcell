df761996ee3ec93de73066f0d22fafc739462886
Regashia <regashia@gmail.com>
2017-06-09 10:32:58 +0900

    Add go-mode

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -64,4 +64,13 @@
 ;;; Markdown
 (require-package 'markdown-preview-mode)
 
+
+;;; Go
+(when (maybe-require-package 'go-mode)
+  (require-package 'company-go)
+  (add-hook 'go-mode-hook
+    (lambda ()
+      (set (make-local-variable 'company-backends) '(company-go))
+      (add-hook 'before-save-hook 'gofmt-before-save))))
+
 (provide 'init-local)
