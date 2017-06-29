4714478a388113a1a8137a520716e967aadcdc2e
Regashia <regashia@gmail.com>
2017-06-29 21:51:33 +0900

    Enable web-mode when editing HTML

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -112,7 +112,8 @@
   (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
 
 
 ;;; eww
